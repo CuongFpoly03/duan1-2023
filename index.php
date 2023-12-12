8f644f62229ff798a918a234665eb07297c20074
HaHongNhung <145672680+HaHongNhung@users.noreply.github.com>
2023-12-12 08:58:37 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,298 +1,238 @@
 <?php
-session_start();
-include "./model/pdo.php";
-include "./model/loai_laptop.php";
-include "./model/san_pham.php";
-include "./model/khach_hang.php";
-include "./model/cart.php";
-include "./model/don_hang.php";
-
-
-$loaidanhmuc = all_list_loai();
-$banner = all_list_namloai();
-$contentdm = all_list_muoiloai();
-$namsp = all_list_namsanpham();
-$sp_noibat = all_list_sanphamnb();
+require_once "../model/loai_laptop.php";
+require_once "../model/san_pham.php";
+require_once "../model/khach_hang.php";
+require_once "../model/binhluan.php";
+require_once "../model/don_hang.php";
+require_once "../model/donhang_ct.php";
+require_once "../model/chart.php";
 
 
 
 $act = $_GET['act'] ?? "";
 switch ($act) {
     case "":
-        $title = "Trang chủ";
-        $VIEW = "./views/layout/home.php";
+        $title = "Tổng Hợp";
+        $title = "Tổng hợp";
+        $totalday = total_ngay();
+        $totalweek  = total_tuan();
+        $totalmonth  = total_thang();
+        $totaldoanhthu  = total_doanhthu();
+        $totalsoluongban  = total_soLuongBan();
+        $sp_bannhieunhat = sanPhamBanNhieuNhat();
+        $slctk = soLuongConTrongKho();
+        $khachhangmoi = soLuongKhachHangMoi();
+        $thoigian = thoiGianMuaNhieuNhat();
+        $VIEW = './layout/home.php';
         break;
-    case 'ctsp':
-        $_SESSION['URI'] = $_SERVER['REQUEST_URI'];
-        // echo "<pre>";
-        // echo $_SESSION['URI'];die;
-        // echo "</pre>";
-        $title = "Chi tiết sản phẩm";
-        $ma_sp = $_GET['ma_sp'];
-        $sanphamct = chitiet_sp($ma_sp);
-        extract($sanphamct);
-        $sp_cungloai = sp_cungloai($ma_sp, $ma_loai);
-        $VIEW = './views/layout/chitietsp.php';
-        break;
-    case 'search':
-        $title = "sản phẩm tìm kiếm";
-        if (isset($_POST['kyw']) && (['kyw'] != "")) {
-            $kyw = $_POST['kyw'];
-        } else {
-            $kyw = "";
-        }
-        if (isset($_GET['ma_loai']) && ($_GET['ma_loai'] > 0)) {
+        //BAI LOAI
+    case "loai":
+        $title = "Danh Sách Loại";
+        if (isset($_GET['ma_loai'])) {
             $ma_loai = $_GET['ma_loai'];
-        } else {
-            $ma_loai = 0;
+            delete_loai($ma_loai);
+            $thongbao = "Xóa dữ liệu thành công!";
         }
-        $listsanpham = loadall_sanpham("$kyw", $ma_loai); //lọc kyw
-        $loaidanhmuc = all_list_loai($ma_loai);
-        $VIEW = './views/layout/searchsp.php';
-        break;
-    case 'locloai':
-        $title = "Loại";
-        if (isset($_GET['ma_loai']) && ($_GET['ma_loai'] > 0)) {
-            $ma_loai = $_GET['ma_loai'];
-        } else {
-            $ma_loai = 0;
-        }
-        $locsanpham = loadall_sanpham("", $ma_loai); //lọc kyw
-        $loaidanhmuc = all_list_loai($ma_loai); //bảng loại
-        $tenloai =  load_ten_loai($ma_loai); // mình tên loại đã extract rồi
-        $VIEW = 'views/layout/locloai.php';
-        break;
-    case 'locnhucau':
-        $title = "Nhu cầu";
-        if (isset($_GET['ma_loai']) && ($_GET['ma_loai'] > 0)) {
-            $ma_loai = $_GET['ma_loai'];
-        } else {
-            $ma_loai = 0;
-        }
-        $locsanpham = loadall_sanpham("", $ma_loai); //lọc kyw
-        $loaidanhmuc = all_list_loai($ma_loai); //bảng loại
 
-        $nhucau =  load_nhu_cau($ma_loai); // mình tên loại đã extract rồi
-        $VIEW = 'views/layout/nhucau.php';
-        break;
-    case 'lockichthuoc':
-        $title = "Kich thước";
-        if (isset($_GET['ma_loai']) && ($_GET['ma_loai'] > 0)) {
-            $ma_loai = $_GET['ma_loai'];
-        } else {
-            $ma_loai = 0;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $ma_loai = $_POST['ma_loai'];
+            delete_loai_item($ma_loai);
+            $thongbao = 'xóa dữ liệu thành công!';
         }
-        $locsanpham = loadall_sanpham("", $ma_loai); //lọc kyw
-        $loaidanhmuc = all_list_loai($ma_loai); //bảng loại
-        $kichthuoc =  load_kich_thuoc($ma_loai); // mình tên loại đã extract rồi
-        $VIEW = 'views/layout/kichthuoc.php';
-        break;
-    case 'locmausac':
-        $title = "Màu sắc";
-        if (isset($_GET['ma_loai']) && ($_GET['ma_loai'] > 0)) {
-            $ma_loai = $_GET['ma_loai'];
-        } else {
-            $ma_loai = 0;
-        }
-        $locsanpham = loadall_sanpham("", $ma_loai); //lọc kyw
-        $loaidanhmuc = all_list_loai($ma_loai); //bảng loại
-        $mausac =  load_mau_sac($ma_loai); // mình tên loại đã extract rồi
-        // var_dump($mausac);die;
 
-        $VIEW = 'views/layout/mausac.php';
+        $listlaptop = all_list_loai();
+        $VIEW = "loai-laptop/list.php";
         break;
-    case 'locram':
-        $title = "Ram";
-        if (isset($_GET['ma_loai']) && ($_GET['ma_loai'] > 0)) {
-            $ma_loai = $_GET['ma_loai'];
-        } else {
-            $ma_loai = 0;
+    case "add":
+        $title = "Thêm Loại";
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $ten_loai = $_POST['ten_loai'];
+            $nhu_cau = $_POST['nhu_cau'];
+            $mau_sac = $_POST['mau_sac'];
+            $kich_thuoc = $_POST['kich_thuoc'];
+            $ram = $_POST['ram'];
+            $luot_xem = $_POST['luot_xem'];
+            $trang_thai = $_POST['trang_thai'] ?? 0;
+            add_loai($ten_loai, $nhu_cau, $mau_sac, $kich_thuoc, $ram, $luot_xem, $trang_thai);
+            setcookie("thongbao", "Thêm Loại thành công!", time() + 1);
+            header("location: ?act=loai");
+            die;
         }
-        $locsanpham = loadall_sanpham("", $ma_loai); //lọc kyw
-        $loaidanhmuc = all_list_loai($ma_loai); //bảng loại
-        $rams =  load_ram($ma_loai); // mình tên loại đã extract rồi
-        $VIEW = 'views/layout/ram.php';
+        $VIEW = "loai-laptop/add.php";
         break;
+    case "update":
+        $title = "Update danh mục";
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $ten_loai = $_POST['ten_loai'];
+            $nhu_cau = $_POST['nhu_cau'];
+            $mau_sac = $_POST['mau_sac'];
+            $kich_thuoc = $_POST['kich_thuoc'];
+            $ram = $_POST['ram'];
+            $luot_xem = $_POST['luot_xem'];
+            $ma_loai = $_POST['ma_loai'];
+            $trang_thai = $_POST['trang_thai'] ?? 0;
+            update_loai($ma_loai, $ten_loai, $nhu_cau, $mau_sac, $kich_thuoc, $ram, $luot_xem, $trang_thai);
+            $thongbao = "Cập nhập dữ liệu thành công";
+        }
+        if (isset($_GET['ma_loai'])) {
+            $ma_loai = $_GET['ma_loai'];
+            $loai = list_one_loai($ma_loai);
+            extract($loai);
+            $VIEW = "loai-laptop/update.php";
+        }
+        break;
+        //BAI SẢN PHẨM LAPTOP
     case 'sanpham':
-        $title = "Sản phẩm mới";
-        $sanpham = all_list_sanpham();
-        $VIEW = 'views/layout/sanpham.php';
-        break;
-    case 'tangdan':
-        $title = "Giá tăng dần";
-        $tangdan = sp_tangdan();
-        $VIEW = 'views/layout/asc.php';
-        break;
-    case 'giamdan':
-        $title = "Giá giảm dần";
-        $giamdan = sp_giamdan();
-        $VIEW = 'views/layout/desc.php';
-        break;
-    case 'banchay':
-        $title = "SP bán chạy";
-        $banchay = sp_banchay();
-        $VIEW = 'views/layout/spbanchay.php';
-        break;
-        //login/logout
-    case "logup":
-        $title = "Đăng ký";
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // var_dump($_POST); die;
-            $email = $_POST['email'];
-            $ten_kh = $_POST['ten_kh'];
-            $mat_khau = $_POST['mat_khau'];
-            add_khachhang($email, $ten_kh, $mat_khau);
-            // setcookie("thongbao", "Thêm Tài khoản thành công!", time() + 1);
-            echo "<script> alert('Bạn đã tạo tài khoản thành công!');</script>";
-            // header("location: ?act=login");
-            // die;
-        }
-        $VIEW = 'views/layout/accounts/signup.php';
-        break;
-    case "login":
-        $title = "Đăng Nhập";
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // var_dump($_POST); die;
-            $ten_kh = $_POST['ten_kh'];
-            $mat_khau = $_POST['mat_khau'];
-            $check_login = check($ten_kh, $mat_khau);
-            if (is_array($check_login)) {
-                $_SESSION['ten_kh'] = $check_login;
-                header("location: ?act=");
-                exit;
-            } else {
-                echo "<script> alert('vui lòng nhâp lại thông tin !');</script>";
-            }
-        }
-        $VIEW = 'views/layout/accounts/login.php';
-        break;
-    case 'thoat':
-        session_unset();
-        header("location: ?act=login");
-        break;
-    case "quenmk":
-        $title = "Quên mk";
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // var_dump($_POST); die;
-            $email = $_POST['email'];
-            $check_email = check_email($email);
-            if (is_array($check_email)) {
-                $thongbao = "Mật khẩu của bạn là:" . $check_email['mat_khau'];
-            } else {
-                $thongbao = "email này k tồn tại";
-            }
-        }
-        $VIEW = 'views/layout/accounts/quenmk.php';
-        break;
-        // gio hang
-    case "cart":
+        $title = "Danh sách sản phẩm";
         if (isset($_GET['ma_sp'])) {
-            $ma_sp = $_GET['ma_sp'] ?? '';
-            $sanpham =  list_one_sp($ma_sp);
-            add_to_cart($sanpham['ma_sp'], $sanpham['ten_sp'], $sanpham['gia_sp'], $sanpham['so_luong'], $sanpham['hinh_sp']);
-        }
-        header("location: " . $_SERVER['HTTP_REFERER']);
-        break;
-    case "viewdonhang":
-        $title = "Đơn hàng";
-        // Xử lý cập nhật số lượng sản phẩm trong giỏ hàng
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_cart'])) {
-            foreach ($_SESSION['cart'] as $key => $product) {
-                if (isset($_POST['quantity_' . $product['ma_sp']])) {
-                    $new_quantity = $_POST['quantity_' . $product['ma_sp']];
-                    // Cập nhật số lượng sản phẩm
-                    $_SESSION['cart'][$key]['so_luong'] = $new_quantity;
-                    // Tính lại thành tiền của sản phẩm
-                    $_SESSION['cart'][$key]['thanh_tien'] = $new_quantity * $product['gia_sp'];
-                }
-            }
-        }
-        // Tính toán tổng tiền của giỏ hàng
-        $total_price = 0;
-        foreach ($_SESSION['cart'] as $product) {
-            $total_price += $product['thanh_tien'];
+            $ma_sp = $_GET['ma_sp'];
+            delete_sp($ma_sp);
+            $thongbao = "Xóa dữ liệu thành công!";
         }
 
-        $carts = $_SESSION['cart'];
-        // var_dump($carts);
-        // die;
-        $sums = sum_cart();
-        $VIEW = "views/layout/cart/viewdonhang.php";
-        break;
-    case 'xoacart':
-        if (isset($_GET['ma_sp'])) {
-            $xoacart = $_GET['ma_sp'];
-            if (isset($_SESSION['cart'][$xoacart])) {
-                unset($_SESSION['cart'][$xoacart]);
-            }
-            header('location: ?act=viewdonhang');
-            break;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $ma_sp = $_POST['ma_sp'];
+            delete_sp_item($ma_sp);
+            $thongbao = 'xóa dữ liệu thành công!';
         }
-    case 'donhang':
-        $title = 'Thông tin thanh toán';
-        $carts = $_SESSION['cart'];
-        $sum = sum_cart();
-        $VIEW = './views/layout/cart/donhang.php';
+
+        $sanpham = all_list_sanpham();
+        $VIEW = "san-pham/list.php";
         break;
-    case 'donhangconfirm':
-        $title = 'Thông tin thanh toán';
-        // điều kện đăng nhập với đặt hàng
-        if (!isset($_SESSION['ten_kh'])) {
-            echo "<script>alert('Mời bạn đăng nhập.');</script>";
-            $VIEW = './views/layout/accounts/login.php'; // Chuyển hướng đến trang đăng nhập
-            break;
-        }
-        // tạo đơn hàng 
+    case 'addsp':
+        $title = "thêm sản phẩm";
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $ten_dh = $_POST['ten_kh'];
-            $email_dh = $_POST['email'];
-            $diachi_dh = $_POST["dia_chi"];
-            $sodt_dh = $_POST['dien_thoai'];
-            $dh_pttt = isset($_POST['dh_pttt']) ? ($_POST['dh_pttt'] === 'one' ? "Thanh toán trực tiếp" : ($_POST['dh_pttt'] === 'two' ? "Thanh toán chuyển khoản" : "không có lựa chọn")) : "";
-            $trang_thai = ($_POST['trang_thai'] === '1' ? "Đang xử lí" : ($_POST['trang_thai'] === '2' ? "Đang giao hàng" : ($_POST['trang_thai'] === '3' ? "Đơn hàng thành công!" : ""))); 
-            $tong_tien = sum_cart();
-            $ngay_dh = date('d/m/Y h:i:sa');
-            $ma_dh = inser_donhang($ten_dh, $diachi_dh, $sodt_dh, $email_dh, $dh_pttt, $tong_tien, $ngay_dh, $trang_thai);
-            // Lưu thông tin chi tiết đơn hàng vào CSDL
-            foreach ($_SESSION['cart'] as $cart) {
-                // Đảm bảo hàm insert_ct_donhang() đã được định nghĩa đúng.
-                insert_ct_donhang($_SESSION['ten_kh']['ma_kh'], $ma_dh, $cart['ma_sp'], $cart['hinh_sp'], $cart['so_luong'], $cart['gia_sp'], $cart['thanh_tien']);
-            }
-            //Xóa giỏ hàng sau khi xử lý đơn hàng
-            $carts = $_SESSION['cart'];
-            $_SESSION['cart'] = [];
+            extract($_POST);
+            $file = $_FILES['hinh_sp'];
+            $hinh_sp = $file['name'];
+            move_uploaded_file($file['tmp_name'], "../views/imgs/" . $hinh_sp);
+            add_sanpham($ten_sp, $gia_sp, $so_luong, $hinh_sp, $mo_ta, $ma_loai);
+            header("location: ?act=sanpham");
+            die;
         }
-        // var_dump($carts); die;
-        $sum = sum_cart();
-        $VIEW = './views/layout/cart/chitietdonhang.php';
+        $loai = all_list_loai();
+        $VIEW = "san-pham/add.php";
         break;
-    case "qldonhang":
-        $title = "Quản lí đơn hàng";
+    case 'updatesp':
+        $title = "Update sản phẩm";
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            extract($_POST);
+            // var_dump($_POST);die;
+            $file = $_FILES['hinh_sp'];
+            //Lấy tên ảnh
+            if ($file['size'] > 0) {
+                $hinh_sp = $file['name'];
+                //Upload ảnh
+                move_uploaded_file($file['tmp_name'], "../views/images/" . $hinh_sp);
+            } else {
+                $hinh_sp = $_POST['hinh_sp'];
+            }
+            update_sp($ma_sp, $ten_sp, $gia_sp, $hinh_sp, $mo_ta, $ma_loai);
+
+            $thongbao = "Cập nhập dữ liệu thành công";
+        }
+        if (isset($_GET['ma_sp'])) {
+            $ma_sp = $_GET['ma_sp'];
+            $sp = list_one_sp($ma_sp);
+            extract($sp);
+            $VIEW = "san-pham/update.php";
+        }
+        $loai = all_list_loai();
+        break;
+        //KHACH HANG
+    case "listkh":
+        $title = 'Danh sách khách hàng';
+        if (isset($_GET['ma_kh'])) {
+            $ma_kh = $_GET['ma_kh'];
+            delete_kh($ma_kh);
+            $thongbao = "Xóa dữ liệu thành công!";
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $ma_kh = $_POST['ma_kh'];
+            delete_kh_item($ma_kh);
+            $thongbao = 'xóa dữ liệu thành công!';
+        }
+        $khachhang = load_all_kh();
+        $VIEW = "khach-hang/list.php";
+        break;
+    case "updatekh":
+        $title = "Sửa khách hàng";
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $ten_kh = $_POST['ten_kh'];
+            $email = $_POST['email'];
+            $mat_khau = $_POST['mat_khau'];
+            $dia_chi = $_POST['dia_chi'];
+            $dien_thoai = $_POST['dien_thoai'];
+            $vai_tro = $_POST['vai_tro'];
+            $ma_kh = $_POST['ma_kh'];
+            update_kh($ma_kh, $ten_kh, $email, $mat_khau, $dia_chi, $dien_thoai, $vai_tro);
+            $thongbao = "Cập nhập dữ liệu thành công";
+        }
+        if (isset($_GET['ma_kh'])) {
+            $ma_kh = $_GET['ma_kh'];
+            $kh = list_one_kh($ma_kh);
+            extract($kh);
+            $VIEW = "Khach-hang/update.php";
+        }
+        break;
+        // binh luan
+    case 'binhluan':
+        $title = "bình luận";
+        if (isset($_GET['ma_bl'])) {
+            $ma_bl = $_GET['ma_bl'];
+            delete_bl($ma_bl);
+            $thongbao = "Xóa dữ liệu thành công!";
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $ma_bl = $_POST['ma_bl'];
+            delete_bl_item($ma_bl);
+            $thongbao = 'xóa dữ liệu thành công!';
+        }
+        $listbinhluan = list_binhluan();
+        $VIEW = "binh-luan/list.php";
+        break;
+    case "listdh":
+        $title = "Đơn hàng";
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (isset($_POST['trang_thai'])) {
-                // var_dump($_POST);die;
-                // $trang_thai = $_POST['trang_thai'];
-                $trang_thai = ($_POST['trang_thai'] === '1' ? "Huỷ đơn hàng" : ""); 
+                // Lấy giá trị trạng thái từ form
+                $trang_thai = $_POST['trang_thai'];
+                $trang_thai = ($_POST['trang_thai'] === '1' ? "Đang xử lí" : ($_POST['trang_thai'] === '2' ? "Đang giao hàng" : ($_POST['trang_thai'] === '3' ? "Đơn hàng thành công!" : ""))); 
                 $ma_dh = isset($_GET['ma_dh']) ? $_GET['ma_dh'] : $_POST['ma_dh'];
-                updateDonHangStatuss($trang_thai, $ma_dh);
-                header("Location: ?act=qldonhang");
+                updateDonHangStatus($trang_thai, $ma_dh);
+                header("Location: ?act=listdh");
                 exit();
             }
-            // if (isset($_GET['ma_dh'])) {
-            //     $ma_kh = $_GET['ma_dh'];
-            //     $dh = load_one_dh($ma_dh);
-            //     extract($dh);
-            //     $VIEW = './views/layout/cart/qldonhang.php';
-            // }
         }
-        $listdonhang = load_all_dh_home();
-        $VIEW = './views/layout/cart/qldonhang.php';
+        $listdh = load_all_dh();
+        $VIEW = "don-hang/list.php";
+        break;
+    case "listdhct":
+        $title = "chi tiết đơn hàng";
+        if (isset($_GET['ma_ct'])) {
+            $ma_ct = $_GET['ma_ct'];
+            delete_ct($ma_ct);
+            $thongbao = "Xóa dữ liệu thành công!";
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $ma_ct = $_POST['ma_ct'];
+            delete_ctdh_item($ma_ct);
+            $thongbao = 'xóa dữ liệu thành công!';
+        }
+        $listct = load_all_ctdh();
+        // var_dump($listdh);
+        $VIEW = "chitietdonhang/list.php";
         break;
     default:
-        echo "./404.php";
-        break;
+        // include "../admin/404.php";
+        echo "../404.php";
 }
 
-include './views/layout/header.php';
+
+
+include './layout/header.php';
 include $VIEW;
-include './views/layout/footer.php';
+include './layout/footer.php';
